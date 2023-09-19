52d1834772e6da42fcc19910d51d9e251027cb3e
RodrigoAraP <133711269+RodrigoAraP@users.noreply.github.com>
2023-09-19 10:23:10 -0300

    Atualizar o Main.java

--- src/ProjetoLotoFacil/Main.java
+++ src/ProjetoLotoFacil/Main.java
@@ -1,17 +1,7 @@
 package ProjetoLotoFacil;
 
-import java.io.IOException;
-
 class Main {
     public static void main(String[] args) {
-        System.out.print("Digite um caractere: ");
-
-        try {
-            char caractere = (char) System.in.read();
-            System.in.skip(1); // Pular a quebra de linha ou qualquer caractere extra após o caractere digitado
-            System.out.println("Você digitou o caractere: " + caractere);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        
     }
 }
